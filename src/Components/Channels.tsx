a5631ff9e32566d3218943fdeb8ce6e875b21a22
MAGHC <windowsapple95@gmail.com>
2022-08-16 17:58:05 +0900

    [add]:message sender 스타일 및 기본 onchange 함수 작성

--- src/Components/Channels.tsx
+++ src/Components/Channels.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 
 const Channels = () => {
-  return <Box height="80vh" width="80vw" bgcolor="black"></Box>;
+  return <Box height="84vh" width="80vw" bgcolor="black"></Box>;
 };
 
 export default Channels;
--- src/Components/SendChat.tsx
+++ src/Components/SendChat.tsx
@@ -1,7 +1,25 @@
-import { Paper, Box } from "@mui/material";
+import { Box, FormControl, Button, TextField } from "@mui/material";
+import { useState } from "react";
 
 const SendChat = () => {
-  return <Box width="80vw" height="20vh" bgcolor="primary.light"></Box>;
+  const [message, setMessage] = useState("");
+
+  const ChangeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setMessage(value);
+    console.log(message);
+  };
+
+  return (
+    <Box display="flex" justifyContent="center" width="80vw" height="16vh" bgcolor="primary.light">
+      <FormControl sx={{ position: "relative", width: "90%", height: "90%" }}>
+        <TextField onChange={ChangeMessage} value={message} sx={{ width: "90%", marginTop: "2rem" }} name="message"></TextField>
+        <Button sx={{ position: "absolute", top: "50%", right: "0%", transform: "translate(0,-50%)" }} variant="contained">
+          전송
+        </Button>
+      </FormControl>
+    </Box>
+  );
 };
 
 export default SendChat;
